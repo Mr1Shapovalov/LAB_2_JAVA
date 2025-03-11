296dbd8d1e64f39e76cc98c2ea1d6f103af46c40
Michailo Shapovalov <mykhailo.sabbath@krfk.kai.edu.ua>
2025-03-11 12:26:02 +0200

    Lab8

---
+++ Lab8/lab8.js
@@ -0,0 +1,7 @@
+// Варіант №17 ,завдання: Створи функцію getFirstCharacter, яка приймає рядок і повертає його перший символ.
+
+function getFirstCharacter(str) {
+    return str.charAt(0);
+}
+console.log(getFirstCharacter("Привіт")); 
+console.log(getFirstCharacter("Світ")); 
