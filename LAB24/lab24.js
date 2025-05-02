11f47a92631c60094a14a359b2503d83c4703185
Michailo Shapovalov <mykhailo.sabbath@krfk.kai.edu.ua>
2025-05-02 10:45:18 +0300

    Lab24

--- LAB24/lab24.js
+++ LAB24/lab24.js
@@ -1,9 +1,16 @@
 // Варіант №17 ,завдання:
 // Додати можливість видалити елемент списку при натисканні на нього.
 
-const list = document.getElementById("myList");
-list.addEventListener("click", function(event) {
+document.getElementById("myList").addEventListener("click", function(event) {
     if (event.target.tagName === "LI") {
         event.target.remove(); 
     }
 });
+
+// Варіант №19 ,завдання:
+// Показувати спливаючу підказку при наведенні на елемент.
+
+const target = document.getElementById('tooltip-target');
+const tooltip = document.getElementById('tooltip');
+target.addEventListener('mouseover', () => tooltip.style.display = 'block');
+target.addEventListener('mouseout', () => tooltip.style.display = 'none');
